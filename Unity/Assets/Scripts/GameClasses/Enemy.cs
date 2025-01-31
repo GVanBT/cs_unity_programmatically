b5e0a828121de9748ade17832785bfb229254ab2
GVanBT <36698@ma-web.nl>
2025-01-31 11:49:01 +0100

    beoordeling files 2

--- Unity/Assets/Scripts/GameClasses/Enemy.cs
+++ Unity/Assets/Scripts/GameClasses/Enemy.cs
@@ -1,23 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-
-
 
 public class Enemy
 {
 //maak hier public class variables:
 	//1 (acces=public, Type= GameObject, name=obj)
-    public ???;
-	//2 (acces=public, Type= int, name=from)
-    public ???;
-	//3 (acces=public, Type= int, name=to)
-    public ???;
+    public GameObject obj;
 
-    public Enemy(???)//verander hier iets zodat je een GameObject met de constructor kan meegeven
+	//2 (acces=public, Type= int, name=from)
+    public int from;
+
+	//3 (acces=public, Type= int, name=to)
+    public int to;
+    private GameObject enemyObj;
+
+    public Enemy(GameObject enemyObj)
+    {
+        this.enemyObj = enemyObj;
+    }
+
+    public Enemy(GameObject obj, int from, int to)///verander hier iets zodat je een GameObject met de constructor kan meegeven
     {
         this.obj = obj;
-		this.from=0;
-		this.to=0;
+        this.from = from;
+        this.to = to;
     }
+
 }
--- Unity/Assets/Scripts/GameClasses/Tower.cs
+++ Unity/Assets/Scripts/GameClasses/Tower.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
+using Unity.VisualScripting;
 using UnityEngine;
 
 //class make oefening
@@ -7,18 +9,27 @@ public class Tower
 {
 //maak hier public class variables:
 	//1  (acces=public, Type= GameObject, name=obj)
-    public ???;
-	//2 (acces=public, Type= GameObject, name=onTile)
-    public ???;
-	//3 (acces=public, Type= float, name=detectRange)
-    public ???;
+    public GameObject obj;
 
-	//verander hier iets zodat je alle variablen mee kan geven aan de constructor
-	//volgorde: obj, detectRange,onTile
-    public Tower(???, ???, ???)
-    {
+	//2 (acces=public, Type= GameObject, name=onTile)
+    public GameObject onTile;
+
+	//3 (acces=public, Type= float, name=detectRange)
+    public float detectRange;
+    private GameObject towerObj;
+    private int v;
+
+    public Tower(GameObject obj, GameObject onTile, float detectRange) //verander hier iets zodat je alle variablen mee kan geven aan de constructor
+    { //volgorde: obj, detectRange,onTile
         this.obj = obj;
+        this.onTile = onTile;
         this.detectRange = detectRange;
+    }
+
+    public Tower(GameObject towerObj, int v, GameObject onTile)
+    {
+        this.towerObj = towerObj;
+        this.v = v;
         this.onTile = onTile;
     }
 }
